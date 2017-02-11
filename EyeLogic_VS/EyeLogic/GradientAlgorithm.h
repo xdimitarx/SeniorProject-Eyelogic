02a16c3d0fa363dca8655664b360fa7d3ce2b823
Pouneh A <Pouneh A>
2017-02-11 11:22:11 -0800

    finish adding in sobel. Next step is to create bins data structure and update it

--- EyeLogic_VS/EyeLogic/GradientAlgorithm.h
+++ EyeLogic_VS/EyeLogic/GradientAlgorithm.h
@@ -15,11 +15,20 @@ using namespace std;
 //Sobel filter kernelx
 float kernelx[3][3] = { { -1,  0,  1 },{ -2,  0,  2 },{ -1, 0, 1 } };
 float kernely[3][3] = { { -1, -2, -1 },{ 0,  0,  0 },{ 1, 2, 1 } };
+int valueThresh = 100;
+
 
 Mat Sobel(Mat eyebox) {
 	float pixel_x, pixel_y;
 	float val;
 	Mat edge(eyebox.rows, eyebox.cols, CV_8UC1);
+	Mat bins(eyebox.rows, eyebox.cols, CV_8UC1);
+	bins = Scalar(0);
+
+
+	//Point 
+	// line( img,	start,		end,		Scalar(0, 0, 0),		thickness,		8 );
+	cout << eyebox.rows << "      " << eyebox.cols << endl;
 
 	for ( int x = 1; x < eyebox.rows - 2; x++) {
 		for (int y = 1; y < eyebox.cols - 2; y ++) {
@@ -32,9 +41,15 @@ Mat Sobel(Mat eyebox) {
 				(kernely[2][0] * eyebox.at<char>(x - 1, y + 1)) + (kernely[2][1] * eyebox.at<char>(x, y + 1)) + (kernely[2][2] * eyebox.at<char>(x + 1, y + 1));
 
 			val = (abs(pixel_x) + abs(pixel_y));
-			edge.at<uchar>(Point(y, x)) = (char)(int)val;
+			if (val > valueThresh) {
+				val = (abs(pixel_x) + abs(pixel_y));
+			}
+			else { val = 0; }
+			edge.at<uchar>(Point(y, x)) = (uchar)(int)val;
 		}
 	}
+
+
 	return edge;
 }
 
@@ -54,8 +69,6 @@ void testSobel() {
 	}
 	return;
 }
-	
-
 
 int gradientAlgo()
 {
@@ -97,33 +110,13 @@ int gradientAlgo()
 
 		//Edge detection
 		//Canny(framegray, canny, lowthresh, lowthresh * 3);
-
-		
-		//Sobel filter implementation. To replace Canny
-		double magX = 0.0; // this is your magnitude
-		int x=30, y=30, width=3; //define x and y as a pixel's coordinates in the eye rectangles (start with framegray), width is of filter
-		for (int a = 0; a < 3; a++)
-		{
-			for (int b = 0; b < 3; b++)
-			{
-				int xn = x + a - 1;
-				int yn = y + b - 1;
-
-				int index = xn + yn * width;
-				magX += framegray.at<uchar>(yn, xn)  * kernelx[a][b];
-			}
-		}
 		
 		//draw rectangles around eyes
 		for (int i = 0; i < eyes.size(); i++) {
 			Rect roiRect = Rect(eyes[i].x, eyes[i].y, eyes[i].width, eyes[i].height);
-			roi = Mat(canny, roiRect);
+			roi = Sobel(Mat(framegray, roiRect));
 			roi.copyTo(framegray.rowRange(eyes[i].y, eyes[i].y + eyes[i].height).colRange(eyes[i].x, eyes[i].x + eyes[i].width));
-			//framegray = Mat(canny, roiRect);
-
-			//rectangle(framegray, Point(eyes[i].x - 1, eyes[i].y - 1), Point(eyes[i].x + eyes[i].width, eyes[i].y + eyes[i].height), Scalar(255));
 		}
-
 		imshow("this is you, smile! :)", framegray);
 		//imshow("this is you, smile! :)", frameColor);
 		if (waitKey(10) == 27) break; // stop capturing by pressing ESC 
