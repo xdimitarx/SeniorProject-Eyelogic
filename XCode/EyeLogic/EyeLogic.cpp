8aa3ee88d52bfc544805fe098cfcb00988cd416d
Pouneh A <Pouneh A>
2017-02-13 16:30:41 -0800

    Revert "fixed "control reaches end of non-void function" error"
    This reverts commit 867d40c7cebac8f3d4bc7c09c850ddf8eeee326c.

--- XCode/EyeLogic/EyeLogic.cpp
+++ XCode/EyeLogic/EyeLogic.cpp
@@ -56,7 +56,7 @@ bool Eye::detectKeyFeatures(Mat input)
     {
         //Blink??
     }
-    return true;
+
 }
 
 bool Eye::getBlink()
@@ -234,7 +234,6 @@ Point ImgFrame:: getCursorXY()
     * To be implemented
     *
     */
-    return Point();
 }
 
 int ImgFrame::getBlink()
