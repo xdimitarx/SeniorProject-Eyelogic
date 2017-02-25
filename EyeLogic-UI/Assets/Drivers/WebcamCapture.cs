169de9af9c7df718df5c9a304ca0cd1701cdb710
Dom <dominikkonecny13@gmail.com>
2017-02-25 03:47:57 -0800

    create driver classes for webcam and mouse

---
+++ EyeLogic-UI/Assets/Drivers/WebcamCapture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public  class WebcamCapture : MonoBehaviour {
+
+    public static WebcamCapture handle;
+
+    public static WebCamTexture webcamTexture;
+    private Color32[] data;
+     
+	// Use this for initialization
+	private void Start () {
+        handle = this;
+        webcamTexture = new WebCamTexture();
+        webcamTexture.Play();
+        data = new Color32[webcamTexture.width * webcamTexture.height];
+    }
+	
+	// Update is called once per frame
+	public Color32[] GetRawImage () {
+        webcamTexture.GetPixels32(data);
+        return data;
+	}
+}
