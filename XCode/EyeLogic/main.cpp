32af102efaa57c21b3c9d1ff9484f08f92226fd3
Dimitar <dimdimvas@yahoo.com>
2017-03-21 17:17:09 -0700

    global reference images are not pointers to MATs anymore

--- XCode/EyeLogic/main.cpp
+++ XCode/EyeLogic/main.cpp
@@ -11,9 +11,9 @@ using namespace std;
 
 // global variables
 Mat ref_topLeft, ref_bottomLeft, ref_center, ref_topRight, ref_bottomRight;
-Mat *refArray [] {new Mat(ref_topLeft), new Mat(ref_bottomLeft), new Mat(ref_center), new Mat(ref_topRight), new Mat(ref_bottomRight)};
+Mat *refArray [] {&ref_topLeft, &ref_bottomLeft, &ref_center, &ref_topRight, &ref_bottomRight};
 int numRefs = 6;
-//Point screenres(1920, 1080);
+Point screenres(1920, 1080);
 std::string filenames [] {"camera.jpg", "topleft.jpg", "bottomleft.jpg", "center.jpg", "topright.jpg", "bottomright.jpg"};
 User *user = nullptr;
 
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
     vector<const EyePair *>reference_vectors;
     
     /* Save user information */
-    std::string user_name = "dimitri";
     fs::path curr_path(fs::current_path());
     std::string imagedir = curr_path.string() + "/images/";
 
@@ -37,67 +36,65 @@ int main(int argc, char *argv[])
      ***************/
     
     // create folder and store reference images
-//    if(!fs::exists(imagedir)){
-//        fs::create_directory(imagedir);
-//        cout << "directory created " << endl;
-//        
-//        std::ofstream outfile(imagedir + "parameters.txt", std::ios::out);
-//
-//        for(int i = 0; i < numRefs; i++){
-//            // take image
-//            Mat camera = cameraCapture();
-//            cout << "next" << endl;
-//            std::string image_path = imagedir + filenames[i];
-//            imwrite(image_path, camera);
-//            ImgFrame camera_frame(screenres);
-//            
-//            // calculate eye Vector
-//            camera_frame.insertFrame(camera);
-//            EyePair pair(camera_frame.getLeftEye().getEyeVector(), camera_frame.getRightEye().getEyeVector());
-//            
-//            // store in file
-//            outfile << pair.leftVector.x << " " << pair.leftVector.y << std::endl;
-//            outfile << pair.rightVector.x << " " << pair.rightVector.y << std::endl;
-//            outfile << std::endl;
-//        }
-//        
-//    }
-//    // if folder already exists, just read in images
-//    else {
-//        std::ifstream inputfile(imagedir + "parameters.txt", std::ios::out);
-//        
-//        for(int i = 0; i < numRefs; i++){
-//            Mat image = imread(imagedir + filenames[i]);
-//            *refArray[i] = image;
-//            
-//            std::string line;
-//            getline(inputfile, line);
-//
-//            std::string x, y;
-//            std::stringstream iss;
-//            iss.str(line);
-//            iss >> x >> y;
-//            Eye leftEye = Eye();
-//            leftEye.leftEye = true;
-//            leftEye.setEyeVector(std::stof(x), std::stof(y));
-//            
-//            iss.clear();
-//            getline(inputfile, line);
-//            iss >> x >> y;
-//            Eye rightEye = Eye();
-//            rightEye.leftEye = false;
-//            rightEye.setEyeVector(std::stof(x), std::stof(y));
-//            
-//        }
-//    }
-    
-    
+    if(!fs::exists(imagedir)){
+        fs::create_directory(imagedir);
+        cout << "directory created " << endl;
+        
+        std::ofstream outfile(imagedir + "parameters.txt", std::ios::out);
+
+        for(int i = 0; i < numRefs; i++){
+            // take image
+            Mat camera = cameraCapture();
+            cout << "next" << endl;
+            std::string image_path = imagedir + filenames[i];
+            imwrite(image_path, camera);
+            ImgFrame camera_frame(screenres);
+            
+            // calculate eye Vector
+            camera_frame.insertFrame(camera);
+            EyePair pair(*camera_frame.getLeftEye().getEyeVector(), *camera_frame.getRightEye().getEyeVector());
+            
+            // store in file
+            outfile << pair.leftVector.x << " " << pair.leftVector.y << std::endl;
+            outfile << pair.rightVector.x << " " << pair.rightVector.y << std::endl;
+            outfile << std::endl;
+        }
+        
+    }
+    // if folder already exists, just read in images
+    else {
+        std::ifstream inputfile(imagedir + "parameters.txt", std::ios::out);
+        
+        for(int i = 0; i < numRefs; i++){
+            Mat image = imread(imagedir + filenames[i]);
+            *refArray[i] = image;
+            
+            std::string line;
+            getline(inputfile, line);
+
+            std::string x, y;
+            std::stringstream iss;
+            iss.str(line);
+            iss >> x >> y;
+            Eye leftEye = Eye();
+            leftEye.leftEye = true;
+            leftEye.setEyeVector(std::stof(x), std::stof(y));
+            
+            iss.clear();
+            getline(inputfile, line);
+            iss >> x >> y;
+            Eye rightEye = Eye();
+            rightEye.leftEye = false;
+            rightEye.setEyeVector(std::stof(x), std::stof(y));
+            
+        }
+    }
     
     /****************
      * Main Program *
      ****************/
     
-    ImgFrame mainEntryPoint(Point(1920,1080));
+    ImgFrame mainEntryPoint(screenres);
     
     if(argc == 2)
     {
